8ab2be4bfc5240c2220f91ce2db71ea61ca1fc03
Sangram Das <121278559+Sangram03@users.noreply.github.com>
2024-05-21 03:49:25 +0530

    Update abc.java
    update

--- abc.java
+++ abc.java
@@ -13,7 +13,7 @@ public static void main (String args[])
 abc a1 = new abc();
 int z = a1.sum(7,6);
 System.out.println("the sum=" +z);
-int y = a1.sum(5,6,7);
+int y = a1.sum(5,226,7);
 System.out.println("the sum=" +y);
 }
 }
