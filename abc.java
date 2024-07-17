9b3a639393011359e957c6e249daf4876e32e829
Sangram Das <121278559+Sangram03@users.noreply.github.com>
2024-07-17 14:21:50 +0530

    Update abc.java
    add

--- abc.java
+++ abc.java
@@ -11,7 +11,7 @@ return a+b+c ;
 public static void main (String args[])
 {
 abc a1 = new abc();
-int z = a1.sum(6,99);
+int z = a1.sum(6,9);
 System.out.println("the sum=" +z);
 int y = a1.sum(5,2,7);
 System.out.println("the sum=" +y);
