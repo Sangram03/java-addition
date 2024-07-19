6d386755234f015c1b5b22ea475386cf20b53b95
Sangram Das <sangramraju143@gmail.com>
2024-07-19 10:37:58 +0530

    hii line error

--- abc.java
+++ abc.java
@@ -13,7 +13,7 @@ public static void main (String args[])
 abc a1 = new abc();
 int z = a1.sum(6,9);
 System.out.println("the sum=" +z);
-int y = a1.sum(5,2,7);
+int y = a1.sum(5,2,9);
 System.out.println("the sum=" +y);
 }
 }
